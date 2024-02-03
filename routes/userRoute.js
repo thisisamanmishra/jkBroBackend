536b83c3da76a36ea47a4897b8e3554ea0424f34
amanmishra151202@gmail.com <amanmishra151202@gmail.com>
2024-02-04 01:57:50 +0530

    Token Bug Fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route("/users/login").post(googleLogin);
 router.route("/user/new").post(createUser);
 // router.route("/login").post(loginUser);
-router.route("/me/:id").put(updateUser)
+router.route("/me/:uid").put(updateUser)
 router.route("/update")
 router.route("/logout").get(logoutUser);
 router.route("/me").get(isAuthenticatedUser, getUser).put(isAuthenticatedUser, updateUser).delete(isAuthenticatedUser, deleteUser);
